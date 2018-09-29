5d9684ed1b5dad9a1903df2aa201724df39de5d2
Joyia Williams <jrwi242@uky.edu>
2018-09-29 12:18:31 -0400

    tweaking code

--- lab-03/quiz-03.py
+++ lab-03/quiz-03.py
@@ -1,14 +1,26 @@
 # Problem 1: "variable names and operators"
-# Print the output of 7 multipled by 56
+# Print the output of 7 multiplied by 56
 # Assign 'OneNumber' a string value
 OneNumber = '7'
 # Assign 'TwoNumber' a string value
+TwoNumber = 56
+print('ONENumber x TwoNumber')
+
+# ----- #
+# Problem 1: "variable names and operators"
+# Print the output of 7 multipled by 56
+# Assign 'OneNumber' a string value
+
+OneNumber = '7'
+# Assign 'TwoNumber' a string value
+
 TwoNumber = 56
 # Turn numbers into  integers
 print(int(7*56))
 
 # Problem 2 "conditional execution and comparison"
 # This should run for any number entered.
+
 x = input("What is your height in feet: ")
 if x == 1:
     print ("For real?")
